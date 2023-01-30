dd34ff6e211061e1b673276738300f7ebf84589d
HIRAM30 <114158681+HIRAM30@users.noreply.github.com>
2023-01-29 23:17:48 -0600

    Add files via upload
    reparacion

---
+++ Untitled-1.js
@@ -0,0 +1,16 @@
+class Reparacion{
+    modedelo;
+    danios;
+    hora_entrada;
+    hora_salida
+    comentarios;
+    constructor(modedelo,danios,hora_entrada,hora_salida,comentarios){
+        this.modedelo=modedelo;
+        this.danios=danios;
+        this.hora_entrada=hora_salida;
+        this.comentarios=comentarios;
+
+
+    }
+
+}
