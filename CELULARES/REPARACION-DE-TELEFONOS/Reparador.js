746bee84aa08e381e798b16228a78654e87e3d3f
HIRAM30 <114158681+HIRAM30@users.noreply.github.com>
2023-02-19 16:05:27 -0600

    Add files via upload

---
+++ CELULARES/REPARACION-DE-TELEFONOS/Reparador.js
@@ -0,0 +1,16 @@
+class Reparador{
+    Nombre;
+    Apellidos;
+    Sueldo;
+    Especialidad;
+    Horarios;
+    constructor(N,A,S,E,H){
+        this.Nombre=N;
+        this.Apellidos=A;
+        this.Sueldo=S;
+        this.Especialidad=E;
+        this.Horarios=H;
+       
+    }
+    
+}
